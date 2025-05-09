fdb6e49e0b2afe9fe9adff892f0f164077ec2532
miui56 <payush706@gmail.com>
2025-05-09 16:39:52 +0000

    rosemary : regenerate sepolicy  fix boot error no more sepolicy_vndr we r using now device sepolicy update vendor_prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
-include device/mediatek/sepolicy_vndr/SEPolicy.mk
+include device/mediatek/sepolicy/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public

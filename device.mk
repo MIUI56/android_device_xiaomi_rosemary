1458ecd909b338fcae26eacea5bc9054b16a49cb
miui56 <payush706@gmail.com>
2025-05-19 16:48:17 +0000

    rosemary: drop unused lib and build required lable some avc

--- device.mk
+++ device.mk
@@ -350,9 +350,10 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.3.vendor
 
 PRODUCT_PACKAGES += \
-    vendor.mediatek.hardware.mtkpower@1.0.vendor \
-    vendor.mediatek.hardware.mtkpower@1.1.vendor \
-    vendor.mediatek.hardware.mtkpower@1.2.vendor
+   android.hardware.power-service.lineage-libperfmgr \
+   vendor.mediatek.hardware.mtkpower@1.2-service.stub \
+   libmtkperf_client_vendor \
+   libmtkperf_client
 
 # PowerOffAlarm
 PRODUCT_PACKAGES += \

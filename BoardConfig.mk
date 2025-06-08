97f2e8c42fcee3502c471d7e724820e813693478
miui56 <payush706@gmail.com>
2025-06-08 02:45:46 +0000

    rosemary:lable some sepolicy denials

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,8 +133,6 @@ TARGET_BOARD_PLATFORM := mt6785
 BOARD_HAS_MTK_HARDWARE := true
 BOARD_HAVE_MTK_FM := true
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop

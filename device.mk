5a4f6a70040ac0a8fc040f43706e54c8fea4d812
miui56 <payush706@gmail.com>
2025-07-18 13:41:55 +0000

    Revert "rosemary: Kill CarrierConfigOverlay"
    This reverts commit ba26bc63da79b8a80bdfaea0671d8dbe6a33162a.

--- device.mk
+++ device.mk
@@ -338,6 +338,7 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlayRosemary \
     FrameworksResOverlayRosemary \
     SettingsProviderOverlayRosemary \
     SystemUIOverlayRosemary \
